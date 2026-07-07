c7566801e38ad050fd11c3bd41818395950c69c5
안우재 <ays97123@gmail.com>
2026-07-07 18:00:57 +0900

    feat: 아이템 슬롯 및 장착 아이템 1차 완성
    머지 전 commit

--- Assets/02_Scripts/InGame/Player/PlayerInventoryViewModel.cs
+++ Assets/02_Scripts/InGame/Player/PlayerInventoryViewModel.cs
@@ -8,10 +8,17 @@ public class HadItemSlotData
 
 public class InventoryModel
 {
+    private int maxInvenCount = 30;
     public List<HadItemSlotData> hadItemSlotDataList { get; private set; } = new();
 
     public void AddItemSlot(string getItemDataId, int getItemCount)
     {
+        if (hadItemSlotDataList.Count >= maxInvenCount)
+        {
+            // TODO: 인벤토리 슬롯이 가득 찼을 때 처리 로직 추가
+
+            return;
+        }
         HadItemSlotData getItemSlotData = new HadItemSlotData();
         // TODO: 데이터 ID string 형태로 변환 시 반영 예정
         /*
@@ -21,6 +28,7 @@ public class InventoryModel
         hadItemSlotDataDictionary.Add(getItemSlotData);
         // TODO: 수량이 Max를 넘긴 상태에서 추가 시, 슬롯을 추가해야 하므로 해당 메서드의 반환형을
         //      bool로 변경하고, GetItem()에서 슬롯 추가 여부를 판단하도록 변경 필요
+        
         */
     }
 
---
+++ Assets/02_Scripts/InGame/Player/PlayerQuickSlotInventoryModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerQuickSlotInventoryModel
+{
+    public HadItemSlotData[] _quickSlots { get; private set; } = new HadItemSlotData[3];
+
+    public HadItemSlotData _equippedWeaponSlot { get; private set; }
+
+    public void EquipQuickSlotItem(HadItemSlotData slotData, int quickSlotIndex)
+    {
+        if (quickSlotIndex < 0 || quickSlotIndex >= _quickSlots.Length)
+        {
+            Debug.LogError("Invalid quick slot index: " + quickSlotIndex);
+            return;
+        }
+
+        _quickSlots[quickSlotIndex] = slotData;
+        _equippedWeaponSlot = slotData;
+    }
+}
+
+public class PlayerQuickSlotInventoryViewModel : ViewModelBase
+{
+    private readonly InventoryModel _inventoryModel;
+    private readonly PlayerQuickSlotInventoryModel _quickSlotModel;
+
+    public IReadOnlyList<HadItemSlotData> InventorySlots => _inventoryModel.hadItemSlotDataList;
+
+    public IReadOnlyList<HadItemSlotData> QuickSlots => _quickSlotModel._quickSlots;
+
+    public HadItemSlotData EquippedWeaponSlot => _quickSlotModel._equippedWeaponSlot;
+
+    public PlayerQuickSlotInventoryViewModel(
+        InventoryModel inventoryModel,
+        PlayerQuickSlotInventoryModel quickSlotModel)
+    {
+        _inventoryModel = inventoryModel;
+        _quickSlotModel = quickSlotModel;
+    }
+
+    public void EquipQuickSlotItem(int inventorySlotIndex, int quickSlotIndex)
+    {
+        if (inventorySlotIndex < 0 || inventorySlotIndex >= InventorySlots.Count)
+            return;
+
+        HadItemSlotData slotData = InventorySlots[inventorySlotIndex];
+
+        if (slotData == null || slotData.itemData == null)
+            return;
+
+        _quickSlotModel.EquipQuickSlotItem(slotData, quickSlotIndex);
+
+        OnPropertyChanged(nameof(QuickSlots));
+        OnPropertyChanged(nameof(EquippedWeaponSlot));
+    }
+}
