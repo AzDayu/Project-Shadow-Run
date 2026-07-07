de19246cccfe89d25f79e80854b321e4e9aeeadc
AzDayu <ehdeh25@gmail.com>
2026-07-07 18:07:06 +0900

    fix:ItemData 수정

--- Assets/02_Scripts/Data/ItemData.cs
+++ Assets/02_Scripts/Data/ItemData.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
 
 [System.Serializable]
 public class ItemData
 {
-    public int ItemId;
+    public string ItemId;
     public string ItemName;
     public string ItemDescription;
-    public int MaxStackCount;
+    public string ItemType;
+    public string Grade;
+    public int MaxStackSize;
+    public int SellingPrice;
 
     public string IconPath;
     public string PrefabPath;
+
+    public string UseItemType;
+    public string[] UseItemParameterList;
 }
 
 [System.Serializable]
