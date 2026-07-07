fe9d63ec8c9ae1400094c5c232e9dd7f4a49669c
안우재 <ays97123@gmail.com>
2026-07-07 11:41:40 +0900

    feat: 플레이어 이동 구현
    InGame 테스트 환경 구성
    InputSystem사용.
    InputSystem → Handler에 입력 저장 → Movement에서 이동 값(방향) 호출 후 속도만큼 이동

---
+++ Assets/02_Scripts/InGame/Player/PlayerInputHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInput _controls;
+
+    public Vector2 MoveInput { get; private set; }
+
+    private void Awake()
+    {
+        _controls = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+
+        _controls.MoveMap.Move2D.performed += OnMove;
+        _controls.MoveMap.Move2D.canceled += OnMoveCanceled;
+    }
+
+    private void OnDisable()
+    {
+        _controls.MoveMap.Move2D.performed -= OnMove;
+        _controls.MoveMap.Move2D.canceled -= OnMoveCanceled;
+
+        _controls.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        MoveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        MoveInput = Vector2.zero;
+    }
+}
---
+++ Assets/02_Scripts/InGame/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerInputHandler InputHandler;
+    // Test : 일시적으로 속도값을 지정
+    [SerializeField] private float MoveSpeed = 5f;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = this.GetComponentInChildren<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 input = InputHandler.MoveInput;
+
+        Vector3 moveDir = new Vector3(input.x, 0f, input.y);
+
+        _rigidbody.linearVelocity = moveDir * MoveSpeed;
+    }
+}
