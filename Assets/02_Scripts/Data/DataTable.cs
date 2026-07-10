1c991963f3460be631b1fb43e6cd8bac4a7f22fd
이광호 <rhkdghgha@gmail.com>
2026-07-10 20:53:18 +0900

    feat : DataManager 추가, ResourceManager 추가 , DataTable 추가

---
+++ Assets/02_Scripts/Data/DataTable.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BaseData
+{
+    public string Id;
+}
+[System.Serializable]
+public class ItemData : BaseData
+{
+    public string ItemId;
+    public string ItemName;
+    public string ItemDescription;
+    public string ItemType;
+    public string Grade;
+    public int MaxStackSize;
+    public int SellingPrice;
+
+    public string IconPath;
+    public string PrefabPath;
+
+    public string UseItemType;
+    public string[] UseItemParameterList;
+}
+
+[System.Serializable]
+public class ItemDataList
+{
+    public ItemData[] Items;
+}
+
+[System.Serializable]
+public class ItemStack
+{
+    public ItemData Item;
+    public int StackCount;
+}
+[System.Serializable]
+public class WeaponData : ItemData
+{
+    public float Damage;
+    public float AttackInterval;
+    public int MagazineSize;
+    public float Accuracy;
+    public float Range;
+    public float ReloadTime;
+}
+[System.Serializable]
+public class WeaponPartsData : ItemData
+{
+    public WeaponPartsType PartsType;
+    public WeaponStatType StatType;
+    public WeaponStatModifierType ModifierType;
+    public float Value;
+}
+[System.Serializable]
+public class PreloadData : BaseData
+{
+    public string Address;
+    public AssetType AssetType;
+
+}
