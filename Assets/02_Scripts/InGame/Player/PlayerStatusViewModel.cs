5366ab4231f1655a92b763e610a63470b1d02e96
안우재 <ays97123@gmail.com>
2026-07-14 17:26:03 +0900

    feat: HP, Stamina HUD 구현

---
+++ Assets/02_Scripts/InGame/Player/PlayerStatusViewModel.cs
@@ -0,0 +1,65 @@
+﻿
+public class PlayerStatusViewModel : ViewModelBase
+{
+    private  PlayerStatusModel _model;
+
+    public float MaxHP => _model.MaxHP;
+    public float CurrentHP => _model.CurrentHP;
+
+    public float MaxStamina => _model.MaxStamina;
+    public float CurrentStamina => _model.CurrentStamina;
+
+    public float HPRatio
+    {
+        get
+        {
+            if (MaxHP <= 0f)
+                return 0f;
+
+            return CurrentHP / MaxHP;
+        }
+    }
+
+    public float StaminaRatio
+    {
+        get
+        {
+            if (MaxStamina <= 0f)
+                return 0f;
+
+            return CurrentStamina / MaxStamina;
+        }
+    }
+
+    public void InitPlayerViewModel(PlayerStatusModel model)
+    {
+        _model = model;
+
+        _model.HealthChanged += OnHealthChanged;
+        _model.StaminaChanged += OnStaminaChanged;
+    }
+
+    private void OnHealthChanged(float currentHP)
+    {
+        OnPropertyChanged(nameof(CurrentHP));
+        OnPropertyChanged(nameof(HPRatio));
+    }
+
+    private void OnStaminaChanged(float currentStamina)
+    {
+        OnPropertyChanged(nameof(CurrentStamina));
+        OnPropertyChanged(nameof(StaminaRatio));
+    }
+
+    public void InvokeOnceOnInit()
+    {
+        OnPropertyChanged(nameof(MaxHP));
+        OnPropertyChanged(nameof(CurrentHP));
+        OnPropertyChanged(nameof(HPRatio));
+
+        OnPropertyChanged(nameof(MaxStamina));
+        OnPropertyChanged(nameof(CurrentStamina));
+        OnPropertyChanged(nameof(StaminaRatio));
+    }
+
+}
---
+++ Assets/02_Scripts/UI/PlayerHUDView.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUDView : UIBase
+{
+    [Header("HP")]
+    [SerializeField] private Slider HPSlider;
+    [SerializeField] private TMP_Text Text_HP;
+
+    [Header("Stamina")]
+    [SerializeField] private Slider StaminaSlider;
+
+    private PlayerStatusViewModel _viewModel;
+
+    public void BindViewModel(PlayerStatusViewModel viewModel)
+    {
+        _viewModel = viewModel;
+
+        _viewModel.PropertyChanged += OnPropertyChanged;
+
+        _viewModel.InvokeOnceOnInit();
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs eventChangeProperty)
+    {
+        switch (eventChangeProperty.PropertyName)
+        {
+            case nameof(PlayerStatusViewModel.HPRatio):
+                HPSlider.value = _viewModel.HPRatio;
+                break;
+
+            case nameof(PlayerStatusViewModel.CurrentHP):
+                break;
+
+            case nameof(PlayerStatusViewModel.MaxHP):
+                UpdateHPText();
+                break;
+
+            case nameof(PlayerStatusViewModel.StaminaRatio):
+                StaminaSlider.value = _viewModel.StaminaRatio;
+                break;
+        }
+    }
+
+    private void UpdateHPText()
+    {
+        Text_HP.text = $"{_viewModel.CurrentHP:0} / {_viewModel.MaxHP:0}";
+    }
+
+    private void OnDestroy()
+    {
+        if (_viewModel == null)
+            return;
+
+        _viewModel.PropertyChanged -= OnPropertyChanged;
+    }
+}
