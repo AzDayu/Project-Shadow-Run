5db7c4e72873b95087eea352abb18508cdaa4de2
leekiin <031317ki@gmail.com>
2026-07-14 11:19:48 +0900

    feat : 서드파티 파일 추가

--- Assets/02_Scripts/OutGame/NetworkStashService.cs
+++ Assets/02_Scripts/OutGame/NetworkStashService.cs
@@ -22,6 +22,24 @@ public class NetworkStashService
         return stashVm;
     }
 
+    public void AddItem(string itemDataId, int addItemCount)
+    {
+        // 저장할때 고유값 ID를 부여하기 위해 사용
+        long uniqueId = GameUtil.GenerateUniqueId();
+
+        // TODO : 우선 쉽게 사용할 수 있도록 중복 처리는 빼두었다. 습득할때마다 아이템이 하나씩 추가되도록 해두고
+        // 추후에 중복값은 StackCount가 다 찰때까지 누적해줄 수 있도록 로직을 추가하자
+        var newItemVm = new StashItemSlotViewModel();
+        newItemVm.ItemUniqueId = uniqueId;
+        newItemVm.ItemDataId = itemDataId;
+        newItemVm.ItemStackCount = addItemCount;
+
+        var stashVm = GetStashViewModel();
+        stashVm.AddItemSlotViewModel(newItemVm);
+
+        //NetworkManager.Inst.SaveLoadService.RequstSaveData();
+    }
+
     private void RequestRemoveItem(long removeTargetUniqueId)
     {
         var stashVm = GetStashViewModel();
